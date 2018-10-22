f5b0a3b772da9013aec4dc39cbe828a4521be797
dmytro <vuyko@ukr.net>
2018-10-23 02:25:54 +0300

    Updates

---
+++ precache-manifest.5e23fdf07adbe22f6d892c90941c89de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d15a28e4939725087320",
+    "url": "/xo/static/js/runtime~main.d15a28e4.js"
+  },
+  {
+    "revision": "17b8df8aa3da8e7ae962",
+    "url": "/xo/static/js/main.17b8df8a.chunk.js"
+  },
+  {
+    "revision": "ccf717e8f1c052517a1a",
+    "url": "/xo/static/js/1.ccf717e8.chunk.js"
+  },
+  {
+    "revision": "17b8df8aa3da8e7ae962",
+    "url": "/xo/static/css/main.6993b86b.chunk.css"
+  },
+  {
+    "revision": "08fd20dbb83ad2a4c7ce366ed84d3f0f",
+    "url": "/xo/index.html"
+  }
+];
